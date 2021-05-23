c1d06726b9248d688a8700fd4d46cf36d66e8874
TCP404 <i@tcp404.com>
2021-05-23 12:51:39 +0800

    Site updated: 2021-05-23 12:51:37

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [["/sw-register.js","00a76a6d04dac17f685b8b21d9ca4bfa"]];
+var precacheConfig = [];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
